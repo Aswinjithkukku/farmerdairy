d1679ee5c8b5486972f4ed04f1a20476fc88e5f7
aswinjithkukku <aswinjitht5@gmail.com>
2024-03-16 01:08:37 +0530

    port of socet io added

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
     pingTimeout: 60000,
     cors: {
-        origin: "http://localhost:3000",
+        origin: `http://localhost:${PORT}`,
         // credentials: true,
     },
 });
